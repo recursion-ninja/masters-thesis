11c21885e5edbd187f32d5e802d09ab80aa98ad8
Alex Washburn <github@recursion.ninja>
2022-04-15 15:47:21 -0400

    Updating the computation and assignment of constants

--- src/mk/constants.mk
+++ src/mk/constants.mk
@@ -38,20 +38,22 @@ basename-constants ::= Parameterized-Constants
 filename-constants ::= $(addsuffix .$(extension-promela),$(basename-constants))
 filepath-constants ::= $(abspath $(addprefix $(dir-protocol-model),$(filename-constants)))
 
-def-pref ::= const-
-def-pair ::= $(COMMA)
+con-pair ::= $(COMMA)
+con-pref ::= const-
+con-temp ::= breif-
+def-pref ::= default-
+sec-pref ::= security-parameter-
 
-default-version ::= 1.0
+$(def-pref)protocol-version ::= 1.0
+$(def-pref)$(sec-pref)T     ::= 12
+$(def-pref)$(sec-pref)C     ::= 12
+$(def-pref)$(sec-pref)N     ::= 8
 
-infostr-glue             ::= _
-infostr-model-name       ::= $(subst $(SPACE),$(infostr-glue),CGKA Model)
-infostr-security-keys    ::= $(subst $(SPACE),$(infostr-glue),T C N)
-infostr-protocol-name    ::= $(subst $(SPACE),$(infostr-glue),TreeKEM v)
-ifndef version
-infostr-protocol-version ::= $(default-version)
-else
-infostr-protocol-version ::= $(version)
-endif
+#######
+###
+#   Custom function definitions for TRANSPILE
+###
+#######
 
 define amend_definitions_within
 	@if [ -n "$(amendment-mapping)" ]; then \
@@ -68,8 +70,91 @@ define amend_definitions_within
 endef
 
 define bits_required
-	$$(shell echo "scale=3; l($(1))/l(2)" | bc -l | cut -f1 -d'.' | xargs -n 1 -I "%" echo "scale=0; " "%" " + 1" | bc)
+$(shell echo "scale=3; l($(1))/l(2)" | bc -l | cut -f1 -d'.' | xargs -n 1 -I "%" echo "scale=0; " "%" " + 1" | bc)
 endef
+
+define security_parameter
+$(if $(strip $($(1))),$($(1)),$(if $(strip $(wildcard $(filepath-constants))),$(shell sed -n 's/^#define \+$(1) \+\([^ ]\+\) *$$/\1/p' $(filepath-constants)),$($(def-pref)$(sec-pref)$(1))))
+endef
+
+#######
+###
+#   Assign the (T,C,N) security parameters variables
+###
+#######
+
+security-parameters ::= T C N
+$(sec-pref)T ::= $(call security_parameter,T)
+$(sec-pref)C ::= $(call security_parameter,C)
+$(sec-pref)N ::= $(call security_parameter,N)
+
+#######
+###
+#   Variables derived from the (T,C,N) security parameters
+###
+#######
+
+# Conditionally assign security parameter values if they were passed from the command line.
+# Compute the bit widths required for the supplied security parameter(s).
+# Compute the constants values which are derivative of the supplied security parameter(s).
+
+# Security parameter: (T)
+$(con-pref)T           ::= $($(sec-pref)T)
+$(con-pref)BITS_T      ::= $(call bits_required,$(shell expr $($(con-pref)T) - 1))
+$(con-pref)BITS_EPOCH  ::= $(call bits_required,$($(con-pref)T))
+$(con-pref)FIRST_EPOCH ::= 0
+$(con-pref)FINAL_EPOCH ::= $(shell expr $($(con-pref)T) - 1)
+$(con-pref)NEVER       ::= $(shell expr $$(( ( 1 << $($(con-pref)BITS_EPOCH) ) - 1 )) )
+
+# Security parameter: (C)
+$(con-pref)C          ::= $($(sec-pref)C)
+$(con-pref)BITS_C     ::= $(call bits_required,$(shell expr $($(con-pref)C) - 1))
+$(con-pref)MAX_REVEAL ::= $(shell expr $($(con-pref)C) - 1)
+
+# Security parameter: (N)
+$(con-pref)N            ::= $($(sec-pref)N)
+$(con-pref)BITS_N       ::= $(call bits_required,$(shell expr $($(con-pref)N) - 1))
+$(con-pref)BITS_USERID  ::= $(call bits_required,$($(con-pref)N))
+$(con-pref)BITS_VERTEX  ::= $(shell expr $($(con-pref)BITS_N) + 1)
+$(con-pref)NONE         ::= $(shell echo $$(( (1 << $($(con-pref)BITS_USERID)) - 1 )) )
+$(con-pref)TREE_ORDER   ::= $(shell echo $$(( (1 << $($(con-pref)BITS_VERTEX)) - 1 )) )
+$(con-pref)ROOT         ::= 0
+$(con-pref)LEAF         ::= $(shell expr $($(con-pref)TREE_ORDER) / 2)
+$(con-pref)FIRST_USERID ::= 0
+$(con-pref)FINAL_USERID ::= $(shell expr $($(con-pref)N) - 1)
+$(con-pref)FIRST_VERTEX ::= 0
+$(con-pref)FINAL_VERTEX ::= $(shell expr $($(con-pref)TREE_ORDER) - 1)
+
+# Collect all defined constant variables and construct a key-value pair mapping
+defined-constants ::= $(sort $(filter $(con-pref)%,$(.VARIABLES)))
+amendment-mapping ::= $(subst $(con-pref),,$(foreach def-const,$(defined-constants),$(def-const)$(con-pair)$($(def-const))))
+
+#######
+###
+#   Variables defining the "Information String"
+###
+#######
+
+ifndef version
+infostr-protocol-version ::= $($(def-pref)protocol-version)
+else
+infostr-protocol-version ::= $(version)
+endif
+
+infostr-glue            ::= _
+infostr-model-name      ::= $(subst $(SPACE),$(infostr-glue),CGKA Model)
+infostr-protocol-name   ::= $(subst $(SPACE),$(infostr-glue),TreeKEM v)
+infostr-security-keys   ::= $(subst $(SPACE),$(infostr-glue),$(security-parameters))
+infostr-security-values ::= \
+    $(subst $(SPACE),$(infostr-glue),$(foreach param,$(security-parameters),$($(sec-pref)$(param))))
+infostr ::= \
+    $(subst $(SPACE),-,$(infostr-model-name) \
+        $(infostr-protocol-name)$(infostr-protocol-version) \
+        $(infostr-security-keys) \
+        $(infostr-security-values))
+
+#$(info ( T, C, N ) = ( '$($(sec-pref)T)', '$($(sec-pref)C)', '$($(sec-pref)N)' ))
+#$(info $(infostr))
 
 #######
 ###
@@ -77,76 +162,11 @@ endef
 ###
 #######
 
-.INTERMEDIATE: assign-constants
-.PHONY: all amend-constants assign-infostr
+.PHONY: all amend-constants
 
 all::;
 
-amend-constants: $(filepath-constants) assign-constants
+amend-constants: $(filepath-constants)
 	$(call amend_definitions_within,$<)
-
-assign-constants:
-#	Conditionally assign security parameter values if they were passed from the command line.
-#	Compute the bit widths required for the supplied security parameter(s).
-#	Compute the constants values which are derivative of the supplied security parameter(s).
-#
-#	Security parameter: (T)
-ifdef T
-#	Process T parameter
-	@$(eval $(def-pref)T     ::= $(T))
-	@$(eval infostr-security ::= "$(infostr-security)T:$(T)")
-#	Compute derivative constants of T
-	@$(eval $(def-pref)BITS_T      ::= $(call bits_required,$(shell expr $($(def-pref)T) - 1)))
-	@$(eval $(def-pref)BITS_EPOCH  ::= $(call bits_required,$($(def-pref)T)))
-	@$(eval $(def-pref)FIRST_EPOCH ::= 0)
-	@$(eval $(def-pref)FINAL_EPOCH ::= $(shell expr $($(def-pref)T) - 1))
-	@$(eval $(def-pref)NEVER       ::= $(shell echo $$(( (1 << $($(def-pref)BITS_EPOCH) ) - 1))))
-endif
-#	Security parameter: (C)
-ifdef C
-#	Process C parameter
-	@$(eval $(def-pref)C     ::= $(C))
-	@$(eval infostr-security ::= "$(infostr-security)C:$(C)")
-#	Compute derivative constants of C
-	@$(eval $(def-pref)BITS_C ::= $(call bits_required,$(shell expr $($(def-pref)C) - 1)))
-	@$(eval $(def-pref)MAX_REVEAL   ::= $(shell expr $($(def-pref)C) - 1))
-endif
-#	Security parameter: (N)
-ifdef N
-#	Process N parameter
-	@$(eval $(def-pref)N     ::= $(N))
-	@$(eval infostr-security ::= "$(infostr-security)N:$(N)")
-#	Compute derivative constants of N
-	@$(eval $(def-pref)BITS_N       ::= $(call bits_required,$(shell expr $($(def-pref)N) - 1)))
-	@$(eval $(def-pref)BITS_USERID  ::= $(call bits_required,$($(def-pref)N)))
-	@$(eval $(def-pref)BITS_VERTEX  ::= $(shell expr $($(def-pref)BITS_N) + 1))
-	@$(eval $(def-pref)NONE         ::= $(shell echo $$(( (1 << $($(def-pref)BITS_USERID)) - 1))))
-	@$(eval $(def-pref)TREE_ORDER   ::= $(shell echo $$(( (1 << $($(def-pref)BITS_VERTEX)) - 1))))
-	@$(eval $(def-pref)ROOT         ::= 0)
-	@$(eval $(def-pref)LEAF         ::= $(shell expr $($(def-pref)TREE_ORDER) / 2))
-	@$(eval $(def-pref)FIRST_USERID ::= 0)
-	@$(eval $(def-pref)FINAL_USERID ::= $(shell expr $($(def-pref)N) - 1))
-	@$(eval $(def-pref)FIRST_VERTEX ::= 0)
-	@$(eval $(def-pref)FINAL_VERTEX ::= $(shell expr $($(def-pref)TREE_ORDER) - 1))
-endif
-#	Collect all defined constant variables and construct a key-value pair mapping
-	@$(eval defined-constants ::= $(sort $(filter $(def-pref)%,$(.VARIABLES))))
-	@$(eval amendment-mapping ::= $(subst $(def-pref),,$(foreach def-const,$(defined-constants),$(def-const)$(def-pair)$($(def-const)))))
-
-
-assign-infostr: $(filepath-constants) amend-constants
-	@$(eval security-vals ::= \
-	$(foreach key,$(subst \
-	    $(infostr-glue),$(SPACE),$(infostr-security-keys)),$(shell \
-	        sed -n 's/^#define $(key) \(.*\) *$$/\1/p' $<)))
-	@$(eval infostr-security-values  ::= $(subst $(SPACE),$(infostr-glue),$(security-vals)))
-	@$(eval infostr-protocol-version ::= $(default-version))
-	@$(eval infostr-all-components   ::= \
-	    $(infostr-model-name) \
-	    $(infostr-protocol-name)$(infostr-protocol-version) \
-	    $(infostr-security-keys) \
-	    $(infostr-security-values))
-	@$(eval infostr ::= $(subst $(SPACE),-,$(infostr-all-components)))
-	$(info $(infostr))
 
 endif # IMPORT_MAKE_CONSTANTS
